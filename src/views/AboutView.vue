6642103d4b6b0c6d69f2662ddfac015c0613a956
taishi <taishiarai@gmail.com>
2024-04-24 17:57:15 +0800

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,4 @@
+<template>
+  <div class="about"></div>
+  <h1>About</h1>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { useCounterStore } from "@/stores/counter";
+
+const storeCounter = useCounterStore()
+
+
+</script>
+
+<template>
+  <div class="home"></div>
+  <div class="count">{{ storeCounter.count }}</div>
+
+  <div class="buttons">
+    <button @click="storeCounter.decreaseCount">-</button>
+    <button @click="storeCounter.increaseCount">+</button>
+  </div>
+  <hr>
+  <div>
+    This counter is: {{ storeCounter.oddOrEven }}
+  </div>
+
+</template>
+
+<style>
+.count {
+  font-size: 60px;
+  margin: 20px;
+}
+
+.buttons button {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
